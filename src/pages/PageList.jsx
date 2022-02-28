223f2bb457b00df2a4a34d74d24607853b51b309
exxocism <tchoi.tab@gmail.com>
2022-02-28 17:04:57 +0900

    Feat: 초기 설정 (#3)
    * Feat: 초기 설정
    
    * Chore: package.json 수정

---
+++ src/pages/PageList.jsx
@@ -0,0 +1,9 @@
+import { useRecoilState } from 'recoil';
+import { userStoredList } from '../atoms';
+
+const PageList = () => {
+  const [userList, setUserList] = useRecoilState(userStoredList);
+  return <div>PageList Hello World!</div>;
+};
+
+export default PageList;
