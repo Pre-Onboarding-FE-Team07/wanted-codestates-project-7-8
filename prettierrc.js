e83850991365cf9a538f27547ca0a8c5174b0342
from97 <skm1104@daum.net>
2022-02-28 15:37:32 +0900

    Chore: Set project environment

---
+++ prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "none",
+  endOfLine: "auto",
+  bracketSpacing: "true", // 대괄호 {} 사이 공백
+  jsxBracketSameLine: "true", // JSX 요소 > 줄바꿈
+};
