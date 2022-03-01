0bd43f0903e25467c8d860b534689ff92174d56a
jeonghun10 <jeonghun8910@gmail.com>
2022-03-01 20:37:30 +0900

    Refactor : Axios Call
    recoil를 이용한 서버 호출 데이터 저장

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,6 +1,20 @@
-import { atom } from 'recoil';
+import { atom, selector } from "recoil";
+import axios from "axios";
 
 export const userStoredList = atom({
-  key: 'userStoredList',
+  key: "userStoredList",
   default: [],
+});
+export const apiDataList = selector({
+  key: "apiDataList",
+  get: async ({ get }) => {
+    const response = await axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://www.chungbuk.go.kr/openapi-json/pubdata/pubMapForest.do"
+      )
+      .then((res) => JSON.stringify(res.data))
+      .catch((err) => console.log(err));
+
+    return response;
+  },
 });
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { RecoilRoot } from 'recoil';
@@ -7,9 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
+    <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </Suspense>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
