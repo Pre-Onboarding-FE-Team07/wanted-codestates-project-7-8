986ac3483cd77dd072566f7ab939b7fb35a9a491
HyoEun Ye <50618754+ye-yo@users.noreply.github.com>
2022-03-02 09:40:44 +0900

    Style: quotes 통일

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,9 +1,9 @@
-import { atom } from "recoil";
-import { localStorageEffect } from "../utilities/localStorage";
+import { atom } from 'recoil';
+import { localStorageEffect } from '../utilities/localStorage';
 import { USER_STORED_LIST } from '../constants/localStorage';
 
 export const userStoredList = atom({
-  key: "userStoredList",
+  key: 'userStoredList',
   default: [],
   effects: [localStorageEffect(USER_STORED_LIST)],
 });
