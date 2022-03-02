77a9fe26d388e72d8af0b63799dd20005431a13c
HyoEun Ye <50618754+ye-yo@users.noreply.github.com>
2022-03-02 09:25:49 +0900

    Feat: Modal component (#6)
    * Design: Modal Component
    
    Modal 컴포넌트 기본 레이아웃 작성
    
    Relted to #2
    
    * Feat: 모달 외부 클릭 처리
    
    모달 외부 클릭 시 close 처리
    
    Related to #2
    
    * Chore: Fix Eslint error
    
    * Chore: Modify index.html
    
    - 사용하지 않는 meta tag 제거
    - tilte 추가 : 충청북도 휴양림 찾기
    
    * Feat: 카드 데이터 적용
    
    카드 데이터 적용 및 디자인 수정
    
    Related to #2
    
    * Feat: Modal 유저리스트 수정, 삭제 기능 구현
    
    - Input 컴포넌트 추가, onChange 이벤트 연결
    - removeData 함수 작성, 삭제 버튼에 연결
    - saveData 함수 작성, 저장/수정 버튼에 연결
    
    * Feat: userStoredList 로컬 스토리지에서 가져와서 초기화하기
    
    * Fix: 코드 정리 및 allowEdit 속성값 변경
    
    Related to #2
    
    * Refactor: localStorage key string 분리 및 코드 정리
    
    * Refactor: localStorage key string 분리 및 코드 정리
    
    * Fix: localstorage 저장 데이터 JSON 직렬화 & 역직렬화
    
    localstorage는 문자열 데이터만 지원하기 때문에 stringify로 직렬화해 저장하고, parse로 파싱해 가져온다.
    
    * Style: single quote 적용
    
    Co-authored-by: from97 <skm1104@daum.net>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-
 import Main from './pages/Main';
 import PageList from './pages/PageList';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/list" element={<PageList />} />
+      <Route path='/' element={<Main />} />
+      <Route path='/list' element={<PageList />} />
     </Routes>
   );
 };
