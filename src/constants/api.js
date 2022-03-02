fc5da904fc703025fd6cde2b959e5baf30defe9e
ye-yo <yeyo0x0@gmail.com>
2022-03-02 22:51:58 +0900

    Chore: URL 주소 변경

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,1 @@
-export const API_URL =
-  'https://cors-anywhere.herokuapp.com/https://www.chungbuk.go.kr/openapi-json/pubdata/pubMapForest.do';
+export const API_URL = 'https://www.chungbuk.go.kr/openapi-json/pubdata/pubMapForest.do';
