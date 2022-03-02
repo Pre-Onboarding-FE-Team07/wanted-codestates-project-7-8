7f0a2fe5658be11b7dfbccf93f78f59970c844bf
jinyongp <dev.jinyongp@gmail.com>
2022-03-02 21:50:50 +0900

    Update: PageList 변경
    - 변수명 변경
    - 불필요한 prop - onClose 제거

--- src/pages/PageList.jsx
+++ src/pages/PageList.jsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { apiDataList } from '../atoms';
@@ -25,29 +25,30 @@ const PageList = () => {
   const userList = useRecoilValue(apiDataList);
   const [page, setPage] = useState(1);
   const [data, setData] = useState(userList);
-  const [selectedData, setSelectedData] = useState(null);
+  const [cardData, setCardData] = useState(null);
 
-  const refetchData = async () => {
+  const refetchData = useCallback(async () => {
     const nextData = await getDataFromApi(page + 1);
-    console.log(nextData);
     if (nextData.length) {
       setData(data.concat(nextData));
       setPage(page + 1);
     }
-  };
+  }, [data, page]);
+
+  const onCardClick = useCallback((card) => {
+    setCardData({ ...card });
+  }, []);
 
   return (
     <Container>
       <GoHomeButton onClick={() => navigate('/')}>⬅</GoHomeButton>
-      {selectedData && (
-        <Modal type="add" cardData={selectedData} onClose={() => setSelectedData(null)} />
-      )}
+      {cardData && <Modal type="add" cardData={cardData} />}
       <CardContainer
         data={data}
         keyExtractor={(item) => item.fcNo.toString()}
         threshold={100}
         onReachScrollEnd={refetchData}
-        renderItem={(data) => <Card cardData={data} onClickEvent={() => setSelectedData(data)} />}
+        renderItem={(data) => <Card cardData={data} onClickEvent={() => onCardClick(data)} />}
       />
     </Container>
   );
